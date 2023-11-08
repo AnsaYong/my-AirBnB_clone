f9a89e87db9a981bf75d4cd80c21ef750eef88b7
AnsaYong <ansahmbomyong@gmail.com>
2023-11-08 20:19:15 +0200

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 import uuid
-import storage
+import models
 from datetime import datetime, timedelta
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         str_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
+            # Not a new instance (from a dictionary representation)
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at" and isinstance(value, str):
                         value = datetime.strptime(value, str_format)
                     setattr(self, key, value)
         else:
+            #  A new instance (not from a dictionary representation)
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
-            new(storage)
+
+            # In addition to the above attributes, load the previous attributes
+            stored_objects = models.storage.all()
+            models.storage.new(stored_objects)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        storage.save()
+        models.storage.save()
         self.updated_at = datetime.today()
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import os
 
 class FileStorage:
     __file_path = "file.json"
@@ -9,20 +10,21 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        key_str = obj.__class__.__name__ + "." + obj.id
+        key_str = obj.__class__.__name__ + "." + "12121212"
         FileStorage.__objects[key_str] = obj
 
     def save(self):
         """
-        Serialize python dictionary
+        Serialize python dictionary - stored in the class attribute `__objects`
         """
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            json.dump(self.all(), f)
+            json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """
-        Deserialize json file
+        Deserialize json file and return it to the dictionary
         """
-        if FileStorage.__file_path:
-            with open(FileStorage.__file_path, "r") as f:
+        file_exists = os.path.exists(FileStorage.__file_path)
+        if file_exists:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 FileStorage.__objects = json.load(f)
